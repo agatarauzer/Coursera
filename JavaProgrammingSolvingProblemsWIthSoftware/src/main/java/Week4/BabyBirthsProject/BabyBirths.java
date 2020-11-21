45ac5a70fd9d80b7ed05f01a258ccbbe581b7d20
agatarauzer <agatarauzer@gmail.com>
2020-11-21 22:10:56 +0100

    baby births class completed

--- JavaProgrammingSolvingProblemsWIthSoftware/src/main/java/Week4/BabyBirthsProject/BabyBirths.java
+++ JavaProgrammingSolvingProblemsWIthSoftware/src/main/java/Week4/BabyBirthsProject/BabyBirths.java
@@ -31,11 +31,11 @@ public class BabyBirths {
             }
         }
         System.out.println("total births: " + totalBirths);
-        System.out.println("total girls births: " + totalBoys);
-        System.out.println("total boys births: " + totalGirls);
+        System.out.println("total girls births: " + totalGirls);
+        System.out.println("total boys births: " + totalBoys);
         System.out.println("total names: " + totalNames);
-        System.out.println("total girls names: " + totalBoysNames);
-        System.out.println("total boys names: " + totalGirlsNames);
+        System.out.println("total girls names: " + totalGirlsNames);
+        System.out.println("total boys names: " + totalBoysNames);
     }
 
     public int getRank(int year, String name, String gender) {
@@ -74,9 +74,65 @@ public class BabyBirths {
         System.out.println(name + " born in " + year + " would be " + newName + " if was born in " + newYear);
     }
 
-    //public int yearOfHighestRank(String name, String gender) {
-       // DirectoryResource dr = new DirectoryResource();
-        //for (File file : dr.selectedFiles()) {
-        //}
+    public int yearOfHighestRank(String name, String gender) {
+       DirectoryResource dr = new DirectoryResource();
+       int maxRank = -1;
+       int yearOfMax = 0;
+       for (File file : dr.selectedFiles()) {
+           String fileName = file.getName();
+           int year = Integer.parseInt(fileName.substring(3,7));
+           int rank = getRank(year, name, gender);
+           if (maxRank == -1) {
+               maxRank = rank;
+               yearOfMax = year;
+           }
+           else {
+               if (rank < maxRank) {
+                       maxRank = rank;
+                       yearOfMax = year;
+               }
+           }
+       }
+       if (maxRank == -1) {
+           return -1;
+       }
+       else {
+           return yearOfMax;
+       }
+    }
+
+    public double getAverageRank(String name, String gender) {
+        DirectoryResource dr = new DirectoryResource();
+        double sum = 0;
+        int counter = 0;
+        for (File file : dr.selectedFiles()) {
+            String fileName = file.getName();
+            int year = Integer.parseInt(fileName.substring(3, 7));
+            int rank = getRank(year, name, gender);
+            sum += rank;
+            counter++;
+        }
+        if (sum < 0) {
+            return -1;
+        }
+        else return  sum / counter;
+    }
+
+    public int getTotalBirthsRankedHigher(int year, String name, String gender) {
+        String fileName = "us_babynames/us_babynames_by_year/yob" + year + ".csv";
+        FileResource fr = new FileResource(fileName);
+        int sumOfBirths = 0;
+        for (CSVRecord record : fr.getCSVParser(false)) {
+            if (record.get(1).equals(gender)) {
+                if (record.get(0).equals(name)) {
+                    return sumOfBirths;
+                }
+                else {
+                    sumOfBirths += Integer.parseInt(record.get(2));
+                }
+
+            }
+        }
+        return sumOfBirths;
     }
 }
