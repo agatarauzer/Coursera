4ec6578c17ed17231287270eb168a8f2d1b35415
agatarauzer <agatarauzer@gmail.com>
2020-11-29 12:02:07 +0100

    word lengths - all methods done

--- JavaProgrammingArraysListsAndStructuredData/src/main/java/Week1/WordLengths.java
+++ JavaProgrammingArraysListsAndStructuredData/src/main/java/Week1/WordLengths.java
@@ -4,7 +4,7 @@ import Week1.predefinedClasses.FileResource;
 
 public class WordLengths {
 
-    public void countWordLengths(FileResource resource, int[] counts) {
+    public int[] countWordLengths(FileResource resource, int[] counts) {
         for (String word : resource.words()) {
             boolean isPrefixALetter = Character.isLetter(word.charAt(0));
             boolean isSufixALetter = Character.isLetter(word.charAt(word.length() - 1));
@@ -20,9 +20,26 @@ public class WordLengths {
             }
             counts[len] += 1;
         }
+        return counts;
+    }
 
-        for (int i = 0; i < counts.length; i++) {
-            System.out.println("words of length " + i + ": " + counts[i]);
+    public void printValues(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                System.out.println("words of length " + i + ": " + array[i]);
+            }
         }
+    }
+
+    public int indexOfMax(int[] values) {
+        int maxValue = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] > maxValue) {
+                maxValue = values[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
     }
 }
