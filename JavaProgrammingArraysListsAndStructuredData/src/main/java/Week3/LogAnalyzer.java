07e0ba821ed54813973aaa629c2aa82492094071
agatarauzer <agatarauzer@gmail.com>
2020-12-16 14:25:25 -0700

    week3, web logs

---
+++ JavaProgrammingArraysListsAndStructuredData/src/main/java/Week3/LogAnalyzer.java
@@ -0,0 +1,27 @@
+package Week3;
+
+import Week1.predefinedClasses.FileResource;
+
+import java.util.ArrayList;
+
+public class LogAnalyzer {
+    private ArrayList<LogEntry> records;
+
+    public LogAnalyzer() {
+        records = new ArrayList<>();
+    }
+
+    public void readFile(String filename) {
+        FileResource fileResource = new FileResource(filename);
+        for (String line : fileResource.lines()) {
+            LogEntry logEntry = WebLogParser.parseEntry(line);
+            records.add(logEntry);
+        }
+    }
+
+    public void printAll() {
+        for (LogEntry le : records) {
+            System.out.println(le);
+        }
+    }
+}
---
+++ JavaProgrammingArraysListsAndStructuredData/src/main/java/Week3/Tester.java
@@ -0,0 +1,18 @@
+package Week3;
+
+import java.util.Date;
+
+public class Tester {
+    public void testLogEntry() {
+        LogEntry le = new LogEntry("1.2.3.4", new Date(), "example request", 200, 500);
+        System.out.println(le);
+        LogEntry le2 = new LogEntry("1.2.100.4", new Date(), "example request 2", 300, 400);
+        System.out.println(le2);
+    }
+
+    public void testLogAnalyzer() {
+        LogAnalyzer logAnalyzer = new LogAnalyzer();
+        logAnalyzer.readFile( "UniqueIPData/short-test_log");
+        logAnalyzer.printAll();
+    }
+}
