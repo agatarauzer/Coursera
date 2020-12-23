313c70fe88e4cb16743976e6695bb1caf1dcd758
agatarauzer <agatarauzer@gmail.com>
2020-12-23 13:23:16 +0100

    vigenere breaker - unknown key length

--- JavaProgrammingArraysListsAndStructuredData/src/main/java/Week4/VigenereBreaker.java
+++ JavaProgrammingArraysListsAndStructuredData/src/main/java/Week4/VigenereBreaker.java
@@ -2,6 +2,9 @@ package Week4;
 
 import Week1.predefinedClasses.FileResource;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class VigenereBreaker {
 
     public String sliceString(String message, int whichSlice, int totalSlices) {
@@ -25,8 +28,54 @@ public class VigenereBreaker {
     public void breakVigenere() {
         FileResource fileResource = new FileResource();
         String message = fileResource.asString();
-        int[] key = tryKeyLength(message, 4, 'e');
+        FileResource fileDictionary = new FileResource();
+        int[] key = tryKeyLength(message, 5, 'e');
+        HashSet<String> dictionary = readDictionary(fileDictionary);
         VigenereCipher vigenereCipher = new VigenereCipher(key);
-        System.out.println(vigenereCipher.decrypt(message));
+        String decrypted = breakForLanguage(message, dictionary);
+        System.out.println(decrypted);
+    }
+
+    public HashSet<String> readDictionary(FileResource fileResource) {
+        HashSet<String> lines = new HashSet<>();
+        for (String line : fileResource.lines()) {
+            lines.add(line.toLowerCase());
+        }
+        return lines;
+    }
+
+    public int countWords(String message, HashSet<String> dictionary) {
+        int counter = 0;
+        String[] words = message.split("\\W+");
+        System.out.println("Total words in file: " + words.length);
+        for (int i = 0; i < words.length; i++) {
+            if (dictionary.contains(words[i].toLowerCase())) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public String breakForLanguage(String encrypted, HashSet<String> dictionary) {
+        int wordsMax = 0;
+        String decrypted = "";
+        int keyLen = 0;
+        int[] correctKey = new int[100];
+        for (int i = 1; i <= 100; i++) {
+            int[] key = tryKeyLength(encrypted, i, 'e');
+            VigenereCipher vigenereCipher = new VigenereCipher(key);
+            String message = vigenereCipher.decrypt(encrypted);
+            int correctWords = countWords(message, dictionary);
+            if (correctWords > wordsMax) {
+                wordsMax = correctWords;
+                decrypted = message;
+                keyLen = i;
+                correctKey = key;
+            }
+        }
+        System.out.println(wordsMax);
+        System.out.println(keyLen);
+        System.out.println(Arrays.toString(correctKey));
+        return decrypted;
     }
 }
