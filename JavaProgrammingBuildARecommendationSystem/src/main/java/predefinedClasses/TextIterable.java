ed2c6fc1e4b3019f06d805e1ce975007c22a8668
agatarauzer <agatarauzer@gmail.com>
2021-04-17 17:37:45 +0200

    Build a recommendation system Course - starter files

---
+++ JavaProgrammingBuildARecommendationSystem/src/main/java/predefinedClasses/TextIterable.java
@@ -0,0 +1,36 @@
+package predefinedClasses;
+
+import java.util.Iterator;
+
+public class TextIterable  implements Iterable<String> {
+    private String[] myStrings;
+
+    /**
+     * Create from a given string.
+     */
+    public TextIterable (String source, String regexp) {
+        myStrings = source.split(regexp);
+    }
+
+    /**
+     * @see java.lang.Iterator
+     */
+    @Override
+    public Iterator<String> iterator () {
+        return new Iterator<String>() {
+            private int myCount = 0;
+
+            @Override
+            public boolean hasNext () {
+                return myCount < myStrings.length;
+            }
+
+            @Override
+            public String next () {
+                String s = myStrings[myCount];
+                myCount++;
+                return s;
+            }
+        };
+    }
+}
