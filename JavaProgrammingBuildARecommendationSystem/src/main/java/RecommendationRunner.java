dcff3b0f712fc3e0d96d8e27aff6c3ecbe1ac916
agata <agatarauzer@gmail.com>
2021-05-13 22:58:36 +0200

    programming exercise: step five - completed RecommendationRunner

--- JavaProgrammingBuildARecommendationSystem/src/main/java/RecommendationRunner.java
+++ JavaProgrammingBuildARecommendationSystem/src/main/java/RecommendationRunner.java
@@ -1,15 +1,17 @@
 import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class RecommendationRunner implements Recommender {
 
     public ArrayList<String> getItemsToRate(){
         GenreFilter genreFilter = new GenreFilter("Drama");
-        YearAfterFilter yearAfterFilter = new YearAfterFilter(2015);
+        YearAfterFilter yearAfterFilter = new YearAfterFilter(2000);
         AllFilters allFilters = new AllFilters();
         allFilters.addFilter(genreFilter);
         allFilters.addFilter(yearAfterFilter);
 
-        int numberOfMovies = 25;
+        int numberOfMovies = 20;
         ArrayList<String> movies = new ArrayList<>();
         for (String movie : MovieDatabase.filterBy(allFilters)) {
            if (numberOfMovies > 0) {
@@ -20,15 +22,64 @@ public class RecommendationRunner implements Recommender {
                break;
            }
         }
+        Collections.sort(movies);
         return movies;
     }
 
     public void printRecommendationsFor(String webRaterID) {
+        MovieDatabase.initialize("ratedmoviesfull.csv");
+        FourthRatings fourthRatings = new FourthRatings();
+        String resultHtml = "";
+        GenreFilter genreFilter = new GenreFilter("Crime");
+        YearAfterFilter yearAfterFilter = new YearAfterFilter(2000);
+        AllFilters allFilters = new AllFilters();
+        allFilters.addFilter(genreFilter);
+        allFilters.addFilter(yearAfterFilter);
 
-        for (String s : getItemsToRate()) {
+        ArrayList<Rating> recommendedMovies = fourthRatings.getSimilarRatingsByFilter(webRaterID, 10, 3, allFilters);
 
+        if (recommendedMovies.isEmpty()) {
+            resultHtml = "No movies to show";
+        }
+        else {
+            resultHtml += " <style>\n" +
+                    "    table{\n" +
+                    "        border:1px solid #000; width:100%;\n" +
+                    "    }\n" +
+                    "    td{\n" +
+                    "        border:1px solid #000; width:100%;\n" +
+                    "    }\n" +
+                    "    </style>" +
+                    "<h2>Recommended crime movies: <h2>\n" +
+                    "<table>\n" +
+                    "<tr>\n" +
+                    "    <th style=\"background-color:#9fd3d4\">Title</th>\n" +
+                    "    <th style=\"background-color:#9fd3d4\">Year</th>\n" +
+                    "    <th style=\"background-color:#9fd3d4\">Average rating</th>\n" +
+                    "</tr>\n";
+
+            int counter = 0;
+            for (Rating rating : recommendedMovies) {
+                String movieId = rating.getItem();
+
+                if (!getItemsToRate().contains(movieId)) {
+                    String title = MovieDatabase.getTitle(movieId);
+                    int year = MovieDatabase.getYear(movieId);
+                    double average = Math.round(rating.getValue());
+
+                    resultHtml += "<tr>\n" +
+                            "    <td>" + title + "</td>\n" +
+                            "    <td>" + year + "</td>\n" +
+                            "    <td>" + average + "</td>\n" +
+                            "  </tr>";
+                }
+                if (++counter > 15) {
+                    break;
+                }
+            }
         }
 
-
+        resultHtml += "</table>";
+        System.out.println(resultHtml);
     }
 }
